bf7975be0f7d4d5c6b63a73cadcc4642dc8a3be3
MrPeperHeeser <valperiya@yandex.ru>
2020-07-26 19:25:29 +0400

    code review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -27,33 +27,6 @@ const addPhotoForm = document.querySelector('.popup__form_add-photo');
 const elementsList = document.querySelector('.elements__list');
 const cardTemplate = document.querySelector('#card').content;
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
 function createCardsListElement(cardName, cardLink) {
     const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
     const img = cardElement.querySelector('.elements__element-img');
